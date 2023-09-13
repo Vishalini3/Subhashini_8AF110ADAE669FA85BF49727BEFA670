f9b5ef2789a76ef6d6ddd9cdc606037839c91dfc
Subhashini2004 <144893136+Vishalini3@users.noreply.github.com>
2023-09-13 20:49:42 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,22 @@
+"""
+1! = 1 * 1
+2! = 2 * 1!->2 * 1
+3! = 3 * 2!->3 * 2 * 1
+.
+.
+10! = 10 * 9 * 8 *.... * 1
+
+formula - n * (n-1)!
+"""
+
+
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+
+number = int(input("Enter a value : "))
+res = fact_rec(number)
+
+print("The factorial of {} is {}".format(number,res))
